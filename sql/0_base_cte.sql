60b4d4b863a4ffd1b0cc58f311ecd1e8e183943f
Vnderbilts <dikiferdianto17@gmail.com>
2026-05-02 11:35:39 +0700

    some fixes

--- sql/0_base_cte.sql
+++ sql/0_base_cte.sql
@@ -15,29 +15,29 @@ WITH employee_base AS (
     state,
     city,
     education_level,
-    TO_DATE(birthdate,              'DD/MM/YYYY')  AS birthdate,
-    TO_DATE(hiredate,               'DD/MM/YYYY')  AS hire_date,
-    TO_DATE(NULLIF(termdate, ''),   'DD/MM/YYYY')  AS term_date,
+    birthdate,
+    hiredate AS hire_date,
+    termdate AS term_date,
     department,
     job_title,
-    salary::numeric                                AS salary,
+    salary::numeric AS salary,
     performance_rating,
 
     CASE
-      WHEN NULLIF(termdate, '') IS NULL THEN 'Hired'
+      WHEN termdate IS NULL THEN 'Hired'
       ELSE 'Terminated'
     END AS employee_status,
 
     EXTRACT(YEAR FROM AGE(
       CURRENT_DATE,
-      TO_DATE(birthdate, 'DD/MM/YYYY')
+      birthdate
     ))::int AS age,
 
     CASE
-      WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, TO_DATE(birthdate, 'DD/MM/YYYY'))) < 25              THEN '<25'
-      WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, TO_DATE(birthdate, 'DD/MM/YYYY'))) BETWEEN 25 AND 34 THEN '25-34'
-      WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, TO_DATE(birthdate, 'DD/MM/YYYY'))) BETWEEN 35 AND 44 THEN '35-44'
-      WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, TO_DATE(birthdate, 'DD/MM/YYYY'))) BETWEEN 45 AND 54 THEN '45-54'
+      WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, birthdate)) < 25              THEN '<25'
+      WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, birthdate)) BETWEEN 25 AND 34 THEN '25-34'
+      WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, birthdate)) BETWEEN 35 AND 44 THEN '35-44'
+      WHEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, birthdate)) BETWEEN 45 AND 54 THEN '45-54'
       ELSE '55+'
     END AS age_group
 
